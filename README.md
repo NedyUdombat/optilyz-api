cce96d9e8f08a00a68c851422b1c75171a502a9b
nedyudombat <nedyudombat@gmail.com>
2021-10-13 12:38:04 +0100

    Setup Linters and Formatters

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Optilyz = A Task manager
 =======
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/29e57bda4b3442576b19/maintainability)](https://codeclimate.com/github/NedyUdombat/optilyz-api/maintainability)
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,10 +1,10 @@
 const jsonResponse = {
-  sendSuccess: (res, status, message = "", data = []) =>
-    res.status(status).json({ status: "success", message, ...data }),
+  sendSuccess: (res, status, message = '', data = []) =>
+    res.status(status).json({ status: 'success', message, ...data }),
   sendError: (res, status, message, data = []) =>
-    res.status(status).json({ status: "error", message: message, ...data }),
+    res.status(status).json({ status: 'error', message: message, ...data }),
   sendFail: (res, status, message) =>
-    res.status(status).json({ status: "fail", message: message }),
+    res.status(status).json({ status: 'fail', message: message }),
 };
 
 export default {
