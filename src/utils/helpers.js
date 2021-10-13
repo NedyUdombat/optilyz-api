e6e4be0c291b97c4e670a3265199ce658cbdb362
nedyudombat <nedyudombat@gmail.com>
2021-10-13 12:42:26 +0100

    Fix hound errors

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,9 +2,9 @@ const jsonResponse = {
   sendSuccess: (res, status, message = '', data = []) =>
     res.status(status).json({ status: 'success', message, ...data }),
   sendError: (res, status, message, data = []) =>
-    res.status(status).json({ status: 'error', message: message, ...data }),
+    res.status(status).json({ status: 'error', message, ...data }),
   sendFail: (res, status, message) =>
-    res.status(status).json({ status: 'fail', message: message }),
+    res.status(status).json({ status: 'fail', message }),
 };
 
 export default {
